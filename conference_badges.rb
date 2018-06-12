aa5e4d63b8f2fc20315803e4c1b316d632083bf2
Yan Wang <wangyan6783@gmail.com>
2018-06-12 17:42:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,8 +7,12 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index{|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  new_array = []
+  array.each_with_index{|name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  new_array
 end
+
+
 
 
 require 'spec_helper'
